061214f26a4a1a5eef936ea16c49b27d77ca0154
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-10-26 19:38:50 +0100

    reverting ptr.cpp example

--- examples/ptr.cpp
+++ examples/ptr.cpp
@@ -2,25 +2,15 @@
 
 int main(){
     bind::ptr<double> sum;
-    bind::ptr< std::tuple<int, int, int> > test;
-
-
-    int a = 10;
-    int b = 20;
-    int c = 30;
+    int a = 10, b = 20;
 
     {
         bind::node first = bind::nodes::begin();
         bind::cpu([](bind::ptr<double>& sum_, int a, int b){
             *sum_ = a + b;
         }, sum, a, b);
-
-        bind::cpu([](bind::ptr< std::tuple<int,int,int> >& test_, int a, int b, int c){
-            *test_ = std::make_tuple(a,b,c);
-        }, test, a, b, c);
     }
 
-    printf("Value : %.2f\n", sum.load());
-    printf("Value : %d %d %d\n", std::get<0>(test.load()),  std::get<1>(test.load()), std::get<2>(test.load()));
+    std::cout << sum << "\n";
     return 0;
 }
