b5786382f6ddc82773aa20822cdcc645c9d558f2
Alex Kosenkov <alex.kosenkov@gmail.com>
2016-01-22 16:44:40 +0100

    inlining reduce

--- examples/reduce.cpp
+++ examples/reduce.cpp
@@ -1,15 +1,18 @@
 #include "utils/bind.hpp"
-#include "utils/reduce.hpp"
 
 int main(){
     const int N = 100;
     std::vector<bind::shared_ptr<int> > a = {1, 2, 3, 4, 6, 7, 8, 9};
 
-    bind::reduce(a, [](bind::shared_ptr<int>& a, const bind::shared_ptr<int>& b){
-        bind::cpu([](bind::shared_ptr<int>& dst, const bind::shared_ptr<int>& src){
-            *dst += *src;
-        }, a, b);
-    });
+    for(int stride = 1; stride < a.size(); stride *= 2){
+        for(int i = stride; i < a.size(); i += stride*2){
+
+            bind::cpu([](bind::shared_ptr<int>& dst, const bind::shared_ptr<int>& src){
+                *dst += *src;
+            }, a[i-stride], a[i]);
+
+        }
+    }
 
     bind::sync();
     std::cout << "Reduced value: " << a[0] << "\n";
